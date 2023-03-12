e914b7e47c67c899ebc947c29cb5e657d76f5430
Thiago Miguel <thiago.miguel@gec.inatel.br>
2023-03-12 11:50:15 -0300

    Update Atividade 2

--- atividade2/lib/main.dart
+++ atividade2/lib/main.dart
@@ -47,37 +47,80 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Counter",
-            style: GoogleFonts.pacifico(fontSize: 30.0, color: Colors.black)),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Click the button to count one more',
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black),
-            ),
-            Text('$_counter', style: GoogleFonts.nabla(fontSize: 70.0)),
-          ],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Counter",
+              style: GoogleFonts.pacifico(fontSize: 30.0, color: Colors.black)),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Click the button to add or remove',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              Text('$_counter', style: GoogleFonts.nabla(fontSize: 70.0)),
+            ],
+          ),
+        ),
+        floatingActionButton: _fab()
 
-      // This trailing comma makes auto-formatting nicer for build methods.
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+
+  Widget _fab() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+        // ignore: prefer_const_constructors
+        SizedBox(
+          width: 5.0,
+        ),
+        FloatingActionButton(
+          onPressed: _decrementCounter,
+          tooltip: 'Decrement',
+          child: const Icon(Icons.remove),
+        ),
+        // ignore: prefer_const_constructors
+        SizedBox(
+          width: 5.0,
+        ),
+        FloatingActionButton(
+          onPressed: _resetCounter,
+          tooltip: 'Reset',
+          child: const Icon(Icons.exposure_zero),
+        ),
+        // ignore: prefer_const_constructors
+        SizedBox(
+          width: 5.0,
+        ),
+      ],
     );
   }
 }
